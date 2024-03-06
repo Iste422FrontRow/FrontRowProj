f5dbf7765616e7c74b7fc6cd1a7d2b438b7281e6
lvl7558 <luka.lasic99@gmail.com>
2024-03-06 09:08:44 -0500

    Added logging to RunEdgeConvert and EdgeTable

--- src/main/java/RunEdgeConvert.java
+++ src/main/java/RunEdgeConvert.java
@@ -1,5 +1,10 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 public class RunEdgeConvert {
+   public static Logger log = LogManager.getLogger(RunEdgeConvert.class);
+
    public static void main(String[] args) {
+      log.info("Application starts");
       EdgeConvertGUI edge = new EdgeConvertGUI();
    }
 }
